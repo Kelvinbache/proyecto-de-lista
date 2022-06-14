0b5ca1e3d847bf6e64792efbd00ac0518162c0fa
kelvin abache <kelvinabache12@gmail.com>
2022-06-14 14:31:02 -0430

    idea de solucion

--- pagina1/todo1/src/add.js
+++ pagina1/todo1/src/add.js
@@ -1,19 +1,39 @@
+'use strict';
+
 /* boton para alladir elemetos */
 
 let ayadirBtn = document.getElementById("btn");
 ayadirBtn.addEventListener("click",ayadir());
 
 
-function ayadir(){
- let caja = []; 
+function ayadir(x,y){
+ y = []; 
  
+ /*crendo elementos */
  for(let i = 0; i <= 2; i++){
   x = prompt("elementos");
-        caja.push(x);      
+        y.push(x);      
 }
-  
-  return console.log(caja);
-
+/*metiebdo informcion */
+ return credorDeTexto(y);
 };
+
+
+function credorDeTexto(j = []){
+let contenedor = document.createElement("div");
+let elementoP = document.createElement("p");
+
+
+/*bucle para recorrer*/
+for(let numero of j){
+let nodoT = document.createTextNode(numero);
+
+/*crear texto*/
+elementoP.appendChild(nodoT);
+contenedor.appendChild(elementoP);
+} 
+ return document.write(contenedor);
+
+}
 
 
